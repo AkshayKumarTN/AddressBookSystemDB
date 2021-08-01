eef89a715a4d30c5789be3790b32681048add407
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-08-01 18:43:09 +0530

    [Akshay] Added -> Created Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,4 +1,15 @@
-----------UC1-->Creating Database-------------
+----------UC1->Creating Database-------------
 create database AddressBookSystem;
 
-
+-------------UC2->Create table------------------
+create table Address_Book_Table
+(
+FirstName varchar(100),
+SecondName varchar(100),
+Address varchar(250),
+City varchar(100),
+State varchar(100),
+zip BigInt,
+PhoneNumber BigInt,
+Email varchar(200)
+)
