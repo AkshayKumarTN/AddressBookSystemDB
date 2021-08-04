ee97c2c01787ae40a330052fadebfc3d2f99dff8
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-08-04 20:25:50 +0530

    [Akshay] Added ->Query to create tables according to ER Diagram

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -45,7 +45,7 @@ add AddressBookName varchar(50),RelationType varchar(50);
 
 update Address_Book_Table set AddressBookName='Friends',RelationType='Friend' where FirstName='Arun' ;
 
-update Address_Book_Table set AddressBookName ='Cousin',RelationType ='Family' where zip=600066;
+update Address_Book_Table set AddressBookName ='Family',RelationType ='Cousin' where zip=600066;
 
 ---------UC10---->Count of persons based on realtion type-----
 Select count(*)as CountType, RelationType  from Address_Book_Table group by RelationType;
@@ -53,4 +53,71 @@ Select count(*)as CountType, RelationType  from Address_Book_Table group by Rela
 ----------UC11----->Insert into tables For each Type--------
 Insert into Address_Book_Table(FirstName,SecondName,Address,City,State,zip,PhoneNumber,Email,AddressBookName,RelationType) 
 values('Vijai','Kumar','T-Nagar','Bengaluru','Karnataka',620068,1234567809,'Vijai@gmail.com','Friends','Friend'),
-('Surya','A','KK-Nagar','Bengaluru','Karnataka',620066,2345678091,'Surya123@gamil.com','Cousin','Family');
+('Surya','A','KK-Nagar','Bengaluru','Karnataka',620066,2345678091,'Surya123@gamil.com','Family','Cousin');
+
+--------------------------UC12-ER Diagram----------------
+CREATE TABLE Contact
+(
+    FirstName VARCHAR(100) NOT NULL,
+    LastName varchar(100) not null,
+    Address varchar(250) not null,
+    City varchar(100) not null,
+    State varchar(100) not null,
+    Zip BigInt not null,
+    PhoneNumber varchar(200) not null
+);
+INSERT into Contact
+VALUES
+('Arun','Kumar','T-Nagar','Chennai','TamilNadu',600068,1234567899),
+('Vignesh','A','ramapuram','Chennai','TamilNadu',600066,2345678991),
+('Vijai','Kumar','T-Nagar','Bengaluru','Karnataka',620068,1234567809),
+('Surya','A','KK-Nagar','Bengaluru','Karnataka',620066,2345678091);
+
+
+create table Contact_Type
+(
+    FirstName varchar(100) not null,
+    RelationType varchar(50) not null
+);
+
+INSERT into Contact_Type
+VALUES
+(
+    'Arun','Friend'
+),
+(
+    'Vignesh','Cousin'
+),
+(
+    'Vijai','Friend'
+),
+(
+    'Surya','Cousin'
+);
+
+
+CREATE TABLE Contact_BookName
+(
+    FirstName VARCHAR(100) NOT NULL,
+    AddressBookName VARCHAR(50) NOT NULL
+)
+INSERT into Contact_BookName
+VALUES
+(
+    'Arun','Friends'
+),
+(
+    'Vignesh','Family'
+),
+(
+    'Vijai','Friends'
+),
+(
+    'Surya','Family'
+);
+
+
+Select * from AddressBookTable;
+Select * from Contact;
+Select * from Contact_Type;
+Select * From Contact_BookName;
