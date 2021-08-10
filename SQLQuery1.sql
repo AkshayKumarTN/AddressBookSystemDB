29244ae4e4c7098aca7ad9abc21370608ba5a155
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-08-10 13:43:51 +0530

    [Akshay] Added ->Aggregate Function On Multiple Tables

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -132,3 +132,30 @@ select  type.RelationType,COUNT(contact.FirstName) as Total_Count
 FROM contact INNER JOIN contact_type type 
 ON contact.FirstName = type.FirstName
 GROUP BY type.RelationType;
+
+-----------------------------
+alter table Contact add
+AddedDate DATE NOT NULL default GETDATE();
+
+------------------------------------------
+
+CREATE PROCEDURE spAddContact  
+(  
+@FirstName varchar(100),
+@LastName varchar(100),
+@Address varchar(250),
+@City varchar(100),
+@State varchar(100),
+@Zip BigInt,
+@PhoneNumber BigInt,
+@Email varchar(200),
+@AddressBookName VARCHAR(50),
+@RelationType varchar(50)
+)  
+as  
+begin  
+        insert into AddressBookTable values(@FirstName,@LastName,@Address,@City,@State,@Zip,@PhoneNumber,@Email,@AddressBookName,@RelationType)
+		insert into Contact values(@FirstName,@LastName,@Address,@City,@State,@Zip,@PhoneNumber,GETDATE())
+        insert into Contact_Type values(@FirstName,@RelationType)
+        insert into Contact_BookName values(@FirstName,@AddressBookName)  
+end 
